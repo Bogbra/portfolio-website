258999c9ff50e4393d9509c2a2d28df5b8107ee3
Bogbra <49477465+Bogbra@users.noreply.github.com>
2024-03-09 15:58:20 +0100

    Update email.php

--- email.php
+++ email.php
@@ -1,14 +1,24 @@
 <?php
 
-$subject = $_POST['name'];
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];  
 
 $recipient = "example@example.com";
 
-$header = "From: <".$email. ">" ;
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+
+$header = "From: $name\n E-Mail: $email\n Message:\n $message";
 
 mail($recipient, $message, $header) or die("Error");
 echo "Mail Sent.";
-
-?>
